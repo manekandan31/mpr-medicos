2ad3f9021804dbd66504c2abbb00b9bccdc8f6c0
Manekandan M <manekandan31@gmail.com>
2026-05-14 19:39:22 +0530

    Update live API URL

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,5 +1,5 @@
 const API_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://your-backend-app.herokuapp.com' // Replace this after deployment
+  ?"https://mpr-medicos-production.up.railway.app"  // Replace this after deployment
   : 'http://127.0.0.1:8000';
 
 export default API_URL;
