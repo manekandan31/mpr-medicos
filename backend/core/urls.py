a3b32d38600f318c5012e7fcc991ea471ef181d3
Manekandan M <manekandan31@gmail.com>
2026-05-15 10:09:56 +0530

    Fix: Force Django to serve media files in production

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -19,7 +19,17 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.urls import re_path
+from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
